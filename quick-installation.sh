de0981cbd2c612e9a5d9ec5cc5c1ca01af7ddce5
Rizwan Fairuz Mamduh <rizwanfairuzmamduh29@gmail.com>
2025-01-05 00:53:52 +0700

    Update quick-installation.sh

--- quick-installation.sh
+++ quick-installation.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 clear
 echo -e "\033[1;32m
@@ -15,20 +15,18 @@ echo -e "\033[1;34m@Ryddd | Testnet, Node Runer, Developer, Retrodrop\033[1;34m"
 sleep 4
 
 # Update & Install dependencies
-echo -e "\033[0;32mUpdating and Installing dependencies...\033[0m"
+echo -e "\033[0;32mUpdating and installing dependencies...\033[0m"
 sudo apt update && sudo apt upgrade -y
 
 # Install packages
-echo -e "\033[0;32mDownload $ Activate Packages...\033[0m"
+echo -e "\033[0;32mDownloading & activating packages...\033[0m"
 curl https://download.hyper.space/api/install | bash
 source ~/.bashrc
-
-#!/bin/bash
 
 # Membuat file hyperspace-key.pem
 KEY_FILE="hyperspace-key.pem"
 
-echo "Masukkan private keys Anda (tekan Enter untuk selesai):"
+echo -e "\033[1;34mMasukkan private keys Anda (tekan Enter untuk selesai):\033[0m"
 read -p "Private Key: " PRIVATE_KEY
 
 # Menulis private key ke dalam file
@@ -36,18 +34,18 @@ echo "$PRIVATE_KEY" > "$KEY_FILE"
 
 # Memastikan file telah dibuat
 if [ -f "$KEY_FILE" ]; then
-    echo "File $KEY_FILE berhasil dibuat."
+    echo -e "\033[1;32mFile $KEY_FILE berhasil dibuat.\033[0m"
 else
-    echo "Gagal membuat file $KEY_FILE. Script dihentikan."
+    echo -e "\033[1;31mGagal membuat file $KEY_FILE. Script dihentikan.\033[0m"
     exit 1
 fi
 
 # Membuat screen session a
-echo "Membuat screen session 'a'..."
-screen -dmS activate-aioscli bash -c "source ~/.bashrc && aios-cli start; exec bash"
+echo -e "\033[1;34mMembuat screen session 'activate-aioscli'...\033[0m"
+screen -dmS activate-aioscli bash -c "source ~/.bashrc && aios-cli start --connect; exec bash"
 
 # Membuat screen session b dengan tambahan perintah
-echo "Membuat screen session 'activate-aioscli-wallet'..."
+echo -e "\033[1;34mMembuat screen session 'start-aioscli-config'...\033[0m"
 screen -dmS start-aioscli-config bash -c "
     source ~/.bashrc &&
     aios-cli hive import-keys ./hyperspace-key.pem &&
@@ -60,4 +58,4 @@ screen -dmS start-aioscli-config bash -c "
     exec bash
 "
 
-echo "Script selesai. Screen sessions 'ctivate-aioscli' dan 'start-aioscli-config' telah dibuat."
+echo -e "\033[1;32mScript selesai. Screen sessions 'activate-aioscli' dan 'start-aioscli-config' telah dibuat.\033[0m"
